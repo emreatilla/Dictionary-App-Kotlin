fc803e31cdadc6b20686908a52c8371bb41829e6
Emre Atilla <emreatilla35@gmail.com>
2022-08-15 14:27:37 +0300

    Nested RecyclerViews implemented

--- app/src/main/java/com/example/dictionaryapp/view/MainActivity.kt
+++ app/src/main/java/com/example/dictionaryapp/view/MainActivity.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.dictionaryapp.databinding.ActivityMainBinding
+import com.example.dictionaryapp.databinding.WordCardBinding
 import com.example.dictionaryapp.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter:RVAdapter
+    private lateinit var adapterMeaning:RVAdapterMeaning
 
     private var _binding : ActivityMainBinding?= null
     private lateinit var binding : ActivityMainBinding
@@ -25,31 +26,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewmodel.refreshData("bank")
-        getLiveData("bank")
+        val dicWord = "bank"
+        viewmodel.refreshData(dicWord)
+        getLiveData()
 
         binding.rvWord.setHasFixedSize(true)
         binding.rvWord.layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
-
     }
 
-    private fun getLiveData(word: String) {
+    private fun getLiveData() {
         viewmodel.dictionary_data.observe(this, Observer { data ->
             data?.let {
                 for (i in 0 until data.size) {
                     val dictionaryModelItem = data[i]
                     val meanings = dictionaryModelItem.meanings
+                    Log.e("DMI", dictionaryModelItem.toString())
+
+                    adapter = RVAdapter(this, data)
+                    binding.rvWord.adapter = adapter
 
                     for (j in meanings.indices) {
                         val meaning = meanings[j]
                         val definitions = meaning.definitions
+                        Log.e("MEANING", meaning.toString())
 
-                        adapter = RVAdapter(this, definitions)
-                        binding.rvWord.adapter = adapter
+                        // adapter = RVAdapter(this, definitions)
+                        // binding.rvWord.adapter = adapter
 
                         for (k in definitions.indices) {
                             val definition = definitions[k]
-                            // Log.e("DATAAA", definition.toString())
+                            Log.e("DEFINITION", definition.toString())
                         }
                     }
                 }
