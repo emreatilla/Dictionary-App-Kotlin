733f9aa469634f649bebec7ce853f69729098b58
Emre Atilla <emreatilla35@gmail.com>
2022-08-17 17:02:45 +0300

    For Multiple Words Counter Added

--- app/src/main/java/com/example/dictionaryapp/view/adapters/RVAdapter.kt
+++ app/src/main/java/com/example/dictionaryapp/view/adapters/RVAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.dictionaryapp.view.adapters
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,11 +16,13 @@ class RVAdapter(private val mContext: Context, private val itemList:List<Diction
 
     inner class CardDesignObjectsHolder(view:View):RecyclerView.ViewHolder(view){
         var textViewWord:TextView
+        var textViewCount:TextView
         var textViewPhonetic:TextView
         var childRV: RecyclerView
 
         init {
             textViewWord = view.findViewById(R.id.twWord)
+            textViewCount = view.findViewById(R.id.twCount)
             textViewPhonetic = view.findViewById(R.id.twPhonetic)
             childRV = view.findViewById(R.id.rvMeaning)
         }
@@ -29,9 +33,13 @@ class RVAdapter(private val mContext: Context, private val itemList:List<Diction
         return CardDesignObjectsHolder(design)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: CardDesignObjectsHolder, position: Int) {
         val def = itemList[position]
 
+        if (itemList.size > 1) {
+            holder.textViewCount.text = "(${position + 1})"
+        }
         holder.textViewWord.text = def.word
         holder.textViewPhonetic.text = def.phonetic
         holder.childRV.setHasFixedSize(true)
