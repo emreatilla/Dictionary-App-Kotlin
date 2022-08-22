e69f29f12056bef4fa6124a5cf6dd09ded06aa8e
Emre Atilla <emreatilla35@gmail.com>
2022-08-22 18:00:24 +0300

    DatabaseHelper.kt Created

---
+++ app/src/main/java/com/example/dictionaryapp/view/db_history/DatabaseHelper.kt
@@ -0,0 +1,20 @@
+package com.example.dictionaryapp.view.db_history
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, "history.sqlite", null, 1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        Log.e("XX_Create", "create")
+        db?.execSQL("CREATE TABLE IF NOT EXISTS histories (\"word_id\" INTEGER PRIMARY KEY AUTOINCREMENT, \"word\" TEXT, \"definition\" TEXT, \"isFlagged\" INTEGER)")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        Log.e("XX_Upgrade", "upgrade")
+        db?.execSQL("DROP TABLE IF EXISTS histories")
+        onCreate(db)
+    }
+
+}
