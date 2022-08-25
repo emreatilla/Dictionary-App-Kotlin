1601eb1b09c65534b1cc882453330f8a32d1075c
Emre Atilla <emreatilla35@gmail.com>
2022-08-25 13:43:18 +0300

    For favorites page methods created on HistoriesDao.kt

--- app/src/main/java/com/example/dictionaryapp/view/db_history/HistoriesDao.kt
+++ app/src/main/java/com/example/dictionaryapp/view/db_history/HistoriesDao.kt
@@ -34,6 +34,30 @@ class HistoriesDao {
         return historyList
     }
 
+    @SuppressLint("Range", "Recycle")
+    fun getFavorites(dbh:DatabaseHelper): ArrayList<Histories> {
+        val historyList = ArrayList<Histories>()
+        val db = dbh.writableDatabase
+        val c = db.rawQuery("SELECT * FROM histories WHERE isFlagged = 1 ORDER BY \"word_id\" DESC", null)
+
+
+        while(c.moveToNext()) {
+            val history = Histories(c.getInt(c.getColumnIndex("word_id")), c.getString(c.getColumnIndex("word")), c.getString(c.getColumnIndex("definition")), c.getString(c.getColumnIndex("speech")), c.getInt(c.getColumnIndex("isFlagged")))
+            historyList.add(history)
+        }
+        return historyList
+    }
+
+    fun addToFavorites(dbh: DatabaseHelper, w: String) {
+        val db = dbh.writableDatabase
+        db.execSQL("UPDATE histories SET isFlagged = 1 WHERE word = \"$w\"")
+    }
+
+    fun removeFavorites(dbh: DatabaseHelper, w: String) {
+        val db = dbh.writableDatabase
+        db.execSQL("UPDATE histories SET isFlagged = 0 WHERE word = \"$w\"")
+    }
+
     fun addWord(dbh: DatabaseHelper, w: String, def: String, speech: String, flag: Int) {
         val db = dbh.writableDatabase
         db.execSQL("INSERT INTO histories (word, definition, speech, isFlagged) VALUES(${w}, ${def}, ${speech}, ${flag})")
