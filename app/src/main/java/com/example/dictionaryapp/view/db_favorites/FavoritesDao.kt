5155938739e8360d018dee515b67722ccb20d298
Emre Atilla <emreatilla35@gmail.com>
2022-09-02 13:44:25 +0300

    FavoritesDao.kt rearranged and fixed

--- app/src/main/java/com/example/dictionaryapp/view/db_favorites/FavoritesDao.kt
+++ app/src/main/java/com/example/dictionaryapp/view/db_favorites/FavoritesDao.kt
@@ -5,18 +5,31 @@ import com.example.dictionaryapp.view.db_history.DatabaseHelper
 
 class FavoritesDao {
 
-    fun addFavorites(dbh: DatabaseHelper, w: String, def: String, speech: String) {
-        val db = dbh.writableDatabase
-        db.execSQL("INSERT INTO favorites (word, definition, speech) VALUES(${w}, ${def}, ${speech})")
+    @SuppressLint("Recycle", "Range")
+    fun addFavorites(dbhf: DatabaseHelperFavorites, dbh: DatabaseHelper, w: String) {
+        val db = dbhf.writableDatabase
+        val dbHistory = dbh.writableDatabase
+        var defString = ""
+        var speString = ""
+        val def = dbHistory.rawQuery("SELECT definition FROM histories WHERE word = \"$w\"", null)
+        while (def.moveToNext()) {
+            defString = def.getString(def.getColumnIndex("definition"))
+        }
+        val speech = dbHistory.rawQuery("SELECT speech FROM histories WHERE word = \"$w\"", null)
+        while (speech.moveToNext()) {
+            speString = speech.getString(speech.getColumnIndex("speech"))
+        }
+
+        db.execSQL("INSERT INTO favorites (word, definition, speech) VALUES(\"${w}\", \"${defString}\", \"${speString}\")")
     }
 
-    fun deleteFavorites(dbh: DatabaseHelper, w: String) {
+    fun deleteFavorites(dbh: DatabaseHelperFavorites, w: String) {
         val db = dbh.writableDatabase
         db.execSQL("DELETE FROM favorites WHERE word = \"$w\"")
     }
 
     @SuppressLint("Range", "Recycle")
-    fun getTenFavorites(dbh:DatabaseHelper): ArrayList<Favorites> {
+    fun getTenFavorites(dbh:DatabaseHelperFavorites): ArrayList<Favorites> {
         val favoritesList = ArrayList<Favorites>()
         val db = dbh.writableDatabase
         val c = db.rawQuery("SELECT * FROM favorites ORDER BY \"word_id\" DESC LIMIT 10", null)
@@ -30,7 +43,7 @@ class FavoritesDao {
     }
 
     @SuppressLint("Range", "Recycle")
-    fun getFavorites(dbh:DatabaseHelper): ArrayList<Favorites> {
+    fun getFavorites(dbh:DatabaseHelperFavorites): ArrayList<Favorites> {
         val favoritesList = ArrayList<Favorites>()
         val db = dbh.writableDatabase
         val c = db.rawQuery("SELECT * FROM favorites ORDER BY \"word_id\" DESC", null)
